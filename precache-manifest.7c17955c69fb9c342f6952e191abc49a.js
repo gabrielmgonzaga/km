7814e47a9f038570bf82302605b05dfa86dc03ec
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2020-03-08 14:20:33 -0700

    Updates

---
+++ precache-manifest.7c17955c69fb9c342f6952e191abc49a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e14b0197085c62b4e08584c0cd926a",
+    "url": "/km/index.html"
+  },
+  {
+    "revision": "7d099ad1c4d91220e264",
+    "url": "/km/static/css/main.daca3416.chunk.css"
+  },
+  {
+    "revision": "1e0b1433b2da5f83f696",
+    "url": "/km/static/js/2.2ba88f04.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/km/static/js/2.2ba88f04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d099ad1c4d91220e264",
+    "url": "/km/static/js/main.df19d99c.chunk.js"
+  },
+  {
+    "revision": "5d241950f38175bfac9a",
+    "url": "/km/static/js/runtime-main.a21e4f6c.js"
+  },
+  {
+    "revision": "aa0f05ab60304686dda1b43f7c276102",
+    "url": "/km/static/media/background.aa0f05ab.jpg"
+  }
+]);
